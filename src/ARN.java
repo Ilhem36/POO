8cef9d8c0794f8b6424c3f07bd53d663cd5970ab
Ellysio <theo.roncalli@yahoo.fr>
2021-03-29 10:02:42 +0200

    Version1

--- src/ARN.java
+++ src/ARN.java
@@ -2,17 +2,26 @@ public class ARN {
 
     // Attributes
     private String sequence;
-    private String appariement;
+    private String matching;
 
     // Constructors
-    public ARN(String sequence, String appariement){
+    public ARN(String sequence, String matching) throws sizeNotCorrectException {
         this.sequence = sequence;
-        this.appariement = appariement;
+        this.matching = matching;
     }
 
     public ARN(String sequence){
         this.sequence = sequence;
-        this.appariement = null;
+        this.matching = null;
+    }
+
+
+
+
+    public static void main(String args[]){
+        ARN seq1 = new ARN("ATCGGCTCGA");
+
+
     }
 
 }
--- src/Presentrna.java
+++
@@ -1,4 +0,0 @@
-public class Presentrna {
-    // on va crée
-
-}
