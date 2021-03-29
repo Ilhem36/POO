383501e416e478ac9dfeec480e3403afe6a693df
Ellysio <theo.roncalli@yahoo.fr>
2021-03-29 11:32:04 +0200

    Premiere version des classes ARN + sizeNotCorrectException

--- src/ARN.java
+++ src/ARN.java
@@ -6,8 +6,13 @@ public class ARN {
 
     // Constructors
     public ARN(String sequence, String matching) throws sizeNotCorrectException {
-        this.sequence = sequence;
-        this.matching = matching;
+        if (sequence.length() != matching.length()){
+            throw new sizeNotCorrectException();
+        }
+        else {
+            this.sequence = sequence;
+            this.matching = matching;
+        }
     }
 
     public ARN(String sequence){
@@ -15,17 +20,47 @@ public class ARN {
         this.matching = null;
     }
 
+    // Methods
+    public String getSequence(){
+        return this.sequence;
+    }
+
+    public String getMatching(){
+        return this.matching;
+    }
+
+    public void setMatching(String matching) throws sizeNotCorrectException {
+        if (this.sequence.length() != matching.length()){
+            throw new sizeNotCorrectException();
+        }
+        else {
+            this.matching = matching;
+        }
+    }
+
+    public String toString(){
+        String result;
+        result = "sequence : " + this.sequence + "\n";
+        if (this.matching != null){
+            result += "matching : " + this.matching;
+        }
+        return result;
+    }
+
+    public String Nussinov(){
+        // Algorithme de Nussinov
+    }
 
 
 
-    public static void main(String args[]){
+
+
+    public static void main(String args[]) throws sizeNotCorrectException {
         ARN seq1 = new ARN("ATCGGCTCGA");
-
+        System.out.println(seq1);
+        seq1.setMatching("((-----))");
+        System.out.println(seq1);
 
     }
-    //class String : la séquence qui est un input
-    //class main : exécuter le programme
-    //class: algorithme nussinov
-    // what do you think Théo le beaugoss?
 
 }
